3b339482e9f0cec31a4895f88fefec923ba5acb4
Hercleiker <hercleiker@gmail.com>
2023-08-02 14:29:54 -0300

    Novo Commit

--- pages/nota/[id].js
+++ pages/nota/[id].js
@@ -7,6 +7,7 @@ export default function GetNota(){
     const router = useRouter()
     const {id} = router.query
     const [nota,setNota] = useState({})
+    const [produtos,setProdutos] = useState([])
 
     useEffect(()=>{
         if(id != null){
@@ -15,15 +16,49 @@ export default function GetNota(){
                 console.log(valor)
                 if(valor['status'] == 'ok'){
                     setNota(valor['result'])
-
+                    setProdutos(valor['result']['produtos'])
                 }
             })
         }
 
     },[router.isReady])
 
+    const Item = ({item}) =>{
+
+       
+  
+        return(
+          <div id="produtos" style={{fontSize:"80%",marginTop:"1%"}}>
+            <label>
+              {item['Item Descricao']}
+            </label>
+  
+            <label style={{marginLeft:"1.5%"}}>
+              {item['Item Codigo']}
+            </label>
+  
+            <label style={{marginLeft:"6%"}}>
+              {item['Item Quantidade']}
+            </label>
+  
+            <label style={{marginLeft:"5%"}}> 
+              R${item['Item Valor']}
+            </label>
+  
+            <label style={{marginLeft:"3%"}}>
+              R${item['Item Total']}
+            </label>
+
+            <label style={{marginLeft:"2%"}}>
+              {item['Item NCM']}
+            </label>
+
+          </div>
+        )
+    }
+
     return(
-        <div>
+        <div id="nota_dados">
             <h3>Nota Fiscal</h3>
 
             <div id="superior">
@@ -47,13 +82,78 @@ export default function GetNota(){
 
             </div>
 
-            <div id="superior" >
-                <h4>Destinatário</h4>
+            <div id="superior" style={{height:"300px"}} >
+                <h4 style={{marginLeft:"2.5%"}}>Destinatário</h4>
+                <div id="parte1">
+                    <p><strong>Contato</strong> : {nota['Contato']}</p>
+                    <p><strong>CPF / CNPJ</strong> : {nota['CPF / CNPJ']}</p>
+                    <p><strong>Fone</strong> : {nota['Fone']}</p>
+                    <p><strong>E-mail</strong> : {nota['E-mail']}</p>
+                </div>
 
-                <p><strong>Contato</strong> : {nota['Contato']}</p>
-                <p><strong>CPF / CNPJ</strong> : {nota['CPF / CNPJ']}</p>
+                <div id="parte2">              
+                    <p><strong>Endereco</strong> : {nota['Endereco']}</p>
+                    <p><strong>Bairro</strong> : {nota['Bairro']}</p>
+                    <p><strong>Municipio</strong> : {nota['Municipio']}</p>
+                </div>
+
+                <div id="parte3">
+                    <p><strong>Número</strong> : {nota['Nro']}</p>
+                    <p><strong>UF</strong> : {nota['UF']}</p>
+                    <p><strong>Complemento</strong> : {nota['Complemento']}</p>
+                </div>
+
+            </div>
+
+            <div style={{marginTop:"2%"}}>
+                <h4>Produtos</h4>
+            <div id="resultados">
+                <div id="labels"  style={{fontSize:"80%"}}>
+
+                <label>
+                    Nome
+                </label>
+
+                <label>
+                   SKU
+                </label>
+
+                <label>
+                    Quantidade
+                </label>
+
+                <label>
+                    Preço Un
+                </label>
+
+                <label>
+                    Total
+                </label>
+
+                <label>
+                    NCM
+                </label>
                 
 
+                </div>
+
+                </div>
+
+                {produtos.map((produto)=>(
+                    <>
+                    <Item item={produto} />
+                    </>
+                ))}
+
+            </div>
+
+
+            <div id="superior">
+            <h3>Valores</h3>
+                <div id="parte1">
+                    <p><strong>Valor Produtos</strong> : R${nota['Valor Produtos']}</p>
+                    <p><strong>Valor Nota</strong> : R${nota['Valor Nota']}</p>
+                </div>
             </div>
 
         </div>
